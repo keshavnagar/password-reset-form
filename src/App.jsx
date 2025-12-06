2f42b10b71ab80e6387d4bdf0e2c78aefc3f58ed
keshavnagar <keshavnagar887@gmail.com>
2025-12-06 18:41:22 +0530

    add html structure without style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { MdLockReset } from "react-icons/md";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { RiLockPasswordFill } from "react-icons/ri";
 const App = () => {
-  return(
+  return (
     <section>
       <div>
-          
+        <MdLockReset />
+        <h1>Create new password</h1>
+        <h2>enter email and new password</h2>
+        <label htmlFor="email">
+          <MdOutlineEmail />
+        </label>
+        <input id="email" type="email" placeholder="enter your email address" />
+        <label htmlFor="password">
+          <RiLockPasswordLine />
+        </label>
+        <input id="password" type="password" placeholder="enter new password" />
+        <label htmlFor="confirmPassword">
+          <RiLockPasswordFill />
+        </label>
+        <input
+          id="confirmPassword"
+          type="password"
+          placeholder="confirm new password"
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 export default App;
